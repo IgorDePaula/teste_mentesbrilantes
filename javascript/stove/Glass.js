e4e4e1533b0ab5446f0a5d77d579c0de3407f980
Igor C. de Paula <principe.borodin@gmail.com>
2024-03-24 17:25:00 -0300

    feat: add Glass to javascript project

---
+++ javascript/stove/Glass.js
@@ -0,0 +1,19 @@
+class Glass {
+    #width;
+    #height;
+
+    constructor(width, height) {
+        this.#width = width;
+        this.#height = height;
+    }
+
+    getWidth() {
+        return this.#width;
+    }
+
+    getHeight() {
+        return this.#height;
+    }
+}
+
+export default Glass

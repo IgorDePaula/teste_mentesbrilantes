c6f1364418cf8dfc119c2cb330c555d6bff68e4c
Igor C. de Paula <principe.borodin@gmail.com>
2024-03-24 17:49:57 -0300

    feat: add default export to javascript project

---
+++ javascript/stove/index.js
@@ -0,0 +1,15 @@
+import Stove from "./Stove";
+import StoveLighter from "./StoveLighter";
+import Firemouth from "./Firemouth";
+import Lamp from "./Lamp";
+import OvenDoor from "./OvenDoor";
+import Button from "./Button";
+
+export default {
+    StoveLighter,
+    Button,
+    Stove,
+    Firemouth,
+    Lamp,
+    OvenDoor
+}

c10b404be407c4708582dd1869bbfe324ed40d42
Igor C. de Paula <principe.borodin@gmail.com>
2024-03-24 17:09:16 -0300

    feat: init javascript project with stove/firemouth

---
+++ javascript/stove/Firemouth.js
@@ -0,0 +1,12 @@
+class Firemouth {
+    #position = ''
+
+    constructor(position) {
+        this.#position = position
+    }
+
+    getPosition() {
+        return this.#position
+    }
+}
+export default Firemouth

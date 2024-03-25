2d1f8fefd345bae26db4f86329392fd5f46eacd9
Igor C. de Paula <principe.borodin@gmail.com>
2024-03-25 10:55:14 -0300

    chore: adjustments on Address and City models

--- laravel/app/Models/Address.php
+++ laravel/app/Models/Address.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['street', 'city_id', 'state_id'];
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
 }
--- laravel/app/Models/City.php
+++ laravel/app/Models/City.php
@@ -15,4 +15,9 @@ class City extends Model
     {
         return $this->belongsTo(State::class);
     }
+
+    public function addresses()
+    {
+        return $this->hasMany(Address::class);
+    }
 }

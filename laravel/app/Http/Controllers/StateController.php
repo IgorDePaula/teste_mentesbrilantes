0f7e87266185cbcb737a3070babe84d8c1a39285
Igor C. de Paula <principe.borodin@gmail.com>
2024-03-25 10:21:52 -0300

    chore: add controller and routes api

--- laravel/app/Http/Controllers/StateController.php
+++ laravel/app/Http/Controllers/StateController.php
@@ -2,49 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreStateRequest;
-use App\Http\Requests\UpdateStateRequest;
 use App\Models\State;
 
 class StateController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        return State::all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreStateRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(State $state)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateStateRequest $request, State $state)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(State $state)
-    {
-        //
+        return $state;
     }
 }

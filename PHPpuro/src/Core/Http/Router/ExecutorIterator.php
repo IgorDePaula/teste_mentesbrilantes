487f13ab62776711153e71f75b8879c1d42b1ac9
Igor C. de Paula <principe.borodin@gmail.com>
2024-03-25 20:16:57 -0300

    chore: start with Router

---
+++ PHPpuro/src/Core/Http/Router/ExecutorIterator.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace MentesBrilhantes\Core\Http\Router;
+
+class ExecutorInterator implements \Iterator
+{
+    protected $elements = [];
+
+    public function __construct(array $elements = [])
+    {
+        $this->elements = $elements;
+    }
+
+
+    public function current()
+    {
+        return current($this->elements);
+    }
+
+
+    public function next()
+    {
+        return next($this->elements);
+    }
+
+
+    public function key()
+    {
+        return key($this->elements);
+    }
+
+
+    public function valid()
+    {
+        return isset($this->elements[current($this->elements)]);
+    }
+
+    public function rewind()
+    {
+        return reset($this->elements);
+    }
+
+    public function count()
+    {
+        return count($this->elements);
+    }
+
+    public function add($element, $name = null)
+    {
+        $this->elements[] = $element;
+    }
+
+    public function remove($element)
+    {
+        foreach ($this->elements as $index => $e) {
+            if ($e == $element) {
+                unset($this->elements[$index]);
+            }
+        }
+    }
+}

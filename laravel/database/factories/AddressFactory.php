5bcaff3e55a8afb7c736ef8a0e5ea7a1d608fd01
Igor C. de Paula <principe.borodin@gmail.com>
2024-03-25 10:49:23 -0300

    chore: Address factory

--- laravel/database/factories/AddressFactory.php
+++ laravel/database/factories/AddressFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\City;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +17,11 @@ class AddressFactory extends Factory
      */
     public function definition(): array
     {
+        $city = City::factory()->create();
         return [
-            //
+            'street' => $this->faker->address(),
+            'city_id' => $city->id,
+            'state_id' => $city->state->id,
         ];
     }
 }
